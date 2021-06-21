91343d66192fb6446f3794e49bdaa73a61832b3d
ManuelEscarlata <7006@itescam.edu.mx>
2021-06-20 19:58:07 -0500

    new

---
+++ Practica2/Java/simulacion.java
@@ -0,0 +1,43 @@
+public void t_InvTiempoServicio3(double num){
+    if(num>=0 && num<=0.05)
+        x=20;
+    if(num>=0.05 && num<=0.15)
+        x=25;
+    if(num>=0.15 && num<=0.35)
+        x=30;
+    if(num>=0.35 && num<=0.60)
+        x=35;
+    if(num>=0.60 && num<=0.72)
+        x=40;
+    if(num>=0.72 && num<=0.82)
+        x=45;
+    if(num>=0.82 && num<=0.90)
+        x=50;
+    if(num>=0.90 && num<=0.96)
+        x=55;
+    if(num>=0.96 && num<=1)
+        x=60;
+    System.out.println(x);
+}  
+
+public void t_InvTiempoServicio4(double num){
+    if(num>=0 && num<=0.05)
+        x=15;
+    if(num>=0.05 && num<=0.20
+        x=20;
+    if(num>=0.20 && num<=0.40)
+        x=25;
+    if(num>=0.40 && num<=0.60)
+        x=30;
+    if(num>=0.60 && num<=0.75)
+        x=35;
+    if(num>=0.75 && num<=0.87)
+        x=40;
+    if(num>=0.87 && num<=0.95)
+        x=45;
+    if(num>=0.95 && num<=0.99)
+        x=50;
+    if(num>=0.99 && num<=1)
+        x=55;
+    System.out.println(x);
+}  
